dc9ce19aa8275dcb3d4abb7d0f04d2169cfea292
shubhii19 <vishwakarmashubhi80@gmail.com>
2025-07-25 00:45:01 +0530

    delete functionality done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,7 @@ import { AppContext } from "../context/AppContext";
 import axios from "axios";
 import HisaabCard from "./HisaabCard";
 import Navbar from "./Navbar";
+import { toast } from "react-toastify";
 
 const Home = () => {
   const { token, setToken, backendUrl } = useContext(AppContext);
@@ -24,6 +25,24 @@ console.log(backendUrl)
     } catch (err) {
       console.log(err.message)
       console.error("Failed to fetch hisaabs:", err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${backendUrl}/hisaab/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      });
+
+      // Update the UI
+      setHisaabs((prev) => prev.filter((item) => item._id !== id));
+      toast.success('Hisaab delete Successfully')
+    } catch (err) {
+      console.error("Failed to delete hisaab", err);
+      alert("Error deleting hisaab");
     }
   };
 
@@ -54,7 +73,7 @@ console.log(backendUrl)
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             {hisaabs.map((item, index) => (
-              <HisaabCard hisaab={item} key={index} />
+              <HisaabCard hisaab={item} key={index}  onDelete={handleDelete} />
             ))}
           </div>
         </div>
